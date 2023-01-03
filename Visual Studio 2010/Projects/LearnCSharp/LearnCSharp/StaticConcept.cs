b6bd8f0b0aeb5f6225869793f3f0065180c87e46
shashank03jan <shashank03jan@gmail.com>
2023-01-03 16:47:49 +0530

    ccc

--- Visual Studio 2010/Projects/LearnCSharp/LearnCSharp/StaticConcept.cs
+++ Visual Studio 2010/Projects/LearnCSharp/LearnCSharp/StaticConcept.cs
@@ -35,4 +35,29 @@ namespace LearnCSharp
         //public  class x1 : y1 { }
         //public static class y1 { }
     }
+   static class StaticConcept1
+    {
+       
+
+        //static method can be private.
+        //see overloading static method. Though it looks like we are overloading it but actually it is not.
+        //because overloading works on instance 
+        private static void method1()
+        {
+        }
+
+        public static void method1(int i)
+        {
+        }
+        public static void method1(string str)
+        {
+        }
+
+        //see inherting is not possible, both as derive as well as base static class
+        //public static class x : y { }
+        //public class y { }
+
+        //public  class x1 : y1 { }
+        //public static class y1 { }
+    }
 }
